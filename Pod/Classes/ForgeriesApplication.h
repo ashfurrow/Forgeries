0cee41e143eba8f848554358a312f039785b5681
Orta Therox <orta.therox@gmail.com>
2016-02-09 10:55:06 +0000

    Add status bar hidden tests to ForgeriesApplication

--- Pod/Classes/ForgeriesApplication.h
+++ Pod/Classes/ForgeriesApplication.h
@@ -3,7 +3,6 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface ForgeriesApplication : UIApplication
-
 @end
 
 NS_ASSUME_NONNULL_END
--- Pod/Classes/ForgeriesApplication.m
+++ Pod/Classes/ForgeriesApplication.m
@@ -3,7 +3,14 @@
 #import <OCMock/OCMock.h>
 
 
+@interface ForgeriesApplication()
+@property(nonatomic,getter=isStatusBarHidden) BOOL statusBarHidden;
+@end
+
 @implementation ForgeriesApplication
+
+@synthesize statusBarHidden=ourStatusBarHidden;
+
 
 /// We can't call the super init function
 /// or it will raise
@@ -11,6 +18,16 @@
 - (instancetype)init
 {
     return self;
+}
+
+- (void)setStatusBarHidden:(BOOL)statusBarHidden
+{
+    ourStatusBarHidden = statusBarHidden;
+}
+
+- (void)setStatusBarHidden:(BOOL)hidden withAnimation:(UIStatusBarAnimation)animation
+{
+    self.statusBarHidden = hidden;
 }
 
 @end
