0df28c967cc6a64691c565233fc7e4c6829760ce
Orta Therox <orta.therox@gmail.com>
2016-02-09 10:39:43 +0000

    Initial work on uiapplication tests

---
+++ Pod/Classes/ForgeriesApplication.m
@@ -0,0 +1,16 @@
+#import "Forgeries-Macros.h"
+#import "ForgeriesApplication.h"
+#import <OCMock/OCMock.h>
+
+
+@implementation ForgeriesApplication
+
+/// We can't call the super init function
+/// or it will raise
+
+- (instancetype)init
+{
+    return self;
+}
+
+@end
